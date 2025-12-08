36a1c9cad34cc959d9692952d4ea7a8250714cc4
Shay Sheller <shaysheller@outlook.com>
2025-12-07 23:34:30 -0800

    finished day7

--- day7/code.js
+++ day7/code.js
@@ -9,18 +9,22 @@ const func = (a) => {
   const visited = new Map();
 
   const dfs = (row, col) => {
-    if (col >= a[0].length || row >= a.length || visited.has(`${row},${col}`)) {
-      return 0;
+    if (row >= a.length) {
+      return 1;
     }
-    visited.set(`${row},${col}`);
+    if (visited.has(`${row},${col}`)) {
+      return visited.get(`${row},${col}`);
+    }
 
     if (a[row][col] === '^') {
       let left = dfs(row + 1, col - 1);
       let right = dfs(row + 1, col + 1);
-      return 1 + left + right;
+      visited.set(`${row},${col}`, left + right);
     } else {
-      return dfs(row + 1, col);
+      let incr = dfs(row + 1, col);
+      visited.set(`${row},${col}`, incr);
     }
+    return visited.get(`${row},${col}`);
   };
 
   result = dfs(0, a[0].indexOf('S'));
@@ -37,6 +41,25 @@ console.log('txt', func(txt));
 
     follow one beam all the way down, and keep a cache of the coordinates of where we've already been 
     and short circuit if we're at a duplicate
+
+
+  part 2 this seems like dp 
+
+    When you hit a square you've been to before you can know how many paths to this point 
+    extend from that path. If you know for example htat hitting one ^ will give you 5 more
+    endings no matter what you acn just increment result by the number of endings that we know
+
+
+  
+  
+  how to actually do part 2:
+
+    dp problem. at each coordinate save how many answers will branch from that path
+
+    if you ever hit the same coord just auto incr the reuslt since there's no reason to
+    compute further
+
+
 
 
 
